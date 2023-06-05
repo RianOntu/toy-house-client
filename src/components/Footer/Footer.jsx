c22fca64df20635e9915ed8146d1010b6831b4f2
RianOntu <rianontu13@gmail.com>
2023-06-05 21:15:49 +0600

    created footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import logo from '../../assets/toy1.png';
+import logo from '../../assets/toy5.webp';
+import './Footer.css'
 
 
 const Footer = () => {
@@ -7,16 +8,26 @@ const Footer = () => {
         <div className='foot'>
                <div className='container'>
             <div className="row">
-                <div className="col-md-4 col-12">
-                <div className='logo d-flex align-items-center'>
+                <div className="col-md-4 col-12 d-flex align-items-center">
+                <div className='logo d-flex align-items-center text-white footer'>
               <img src={logo} alt="" />
                <h3>Toy House</h3>
             
 
         </div>
                 </div>
-                <div className="col-md-4 col-12"></div>
-                <div className="col-md-4 col-12"></div>
+                <div className="col-md-4 col-12 d-flex text-white align-items-center">
+                &copy; Rian Ontu all rights reserved
+                </div>
+                <div className="col-md-4 col-12 text-white ">
+                    <p>Phone:121212 <br /> California,United States</p>
+                    <div className="d-flex">
+                    <i class="fa-brands fa-facebook mr-3"></i>
+                    <i class="fa-brands fa-instagram mr-3"></i>
+                    <i class="fa-brands fa-twitter"></i>
+                    </div>
+
+                </div>
             </div>
             
         </div>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../../assets/toy1.png';
+import logo from '../../assets/toy5.webp';
 import './Header.css'
 import { Link } from 'react-router-dom';
 
@@ -22,6 +22,9 @@ const Header = () => {
           <ul class="navbar-nav ml-auto">
             <li class="nav-item">
               <Link to='/'>Home</Link>
+            </li>
+            <li class="nav-item">
+              <Link to='/blog'>Blog</Link>
             </li>
            
            
